8374fa037fffa5c3c9e4546477e7182bbc37a1d5
abhinav-fairsettle <abhinavxeon@gmail.com>
2024-09-10 21:24:07 +0530

    team add

---
+++ java/out/production/java/Test.java
@@ -0,0 +1,9 @@
+package out.production.java;
+
+public class Test {
+    public static void main(String[] args) {
+        int a=5;
+        int b=6;
+        System.out.println("hello"+(a+b));
+    }
+}
