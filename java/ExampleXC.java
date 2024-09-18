7ee00eb9aaca44445be1b58da31ea8f0af0e7512
abhinav-fairsettle <abhinavxeon@gmail.com>
2024-09-18 21:05:08 +0530

    sample program

--- java/ExampleXC.java
+++ java/ExampleXC.java
@@ -1,13 +1,42 @@
 public class ExampleXC {
 
+     boolean b1;
+     byte b2;
+     short s;
+     int a;
+     long b;
+     float f;
+     double d;
+     String name;
+
+
+     void metho1(){
+
+         System.out.println(b1); //false
+         System.out.println(b2);//0
+         System.out.println(s);//0
+         System.out.println(a);//0
+         System.out.println(b);//0.0
+         System.out.println(f);//0.0
+         System.out.println(d);//0.0
+         System.out.println(name);//null
+
+
+     }
+
 
     public static void main(String[] args) {
 
 
-        System.out.println("abhinav");
+        ExampleXC obj = new ExampleXC();
+        obj.metho1();
+
+
+
 
 
     }
 
 
 }
+
---
+++ java/out/production/java/SampleTest.java
@@ -0,0 +1,41 @@
+package out.production.java;
+
+public class SampleTest {
+
+  int a,b,c=7;
+
+
+
+   void show(){
+
+
+
+       int x=0;
+       System.out.println(x);
+
+   }
+
+
+    public static void main(String[] args) {
+
+        SampleTest obj = new SampleTest();
+
+        System.out.println(obj.val);
+
+
+        int a,b,c;
+        a=b=c=8;
+
+        final int ax=10;
+
+
+
+
+
+
+
+    }
+
+
+
+}
