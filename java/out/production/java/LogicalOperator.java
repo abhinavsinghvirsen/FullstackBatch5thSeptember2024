f5a12b9a2fcaa92479b495a8e2f97c4437181b6a
abhinav-fairsettle <abhinavxeon@gmail.com>
2024-10-03 21:27:52 +0530

    operator

---
+++ java/out/production/java/LogicalOperator.java
@@ -0,0 +1,62 @@
+package out.production.java;
+
+public class LogicalOperator {
+
+    public static void main(String[] args) {
+
+        //int a=10;
+
+
+       // System.out.println(!a);
+
+        //System.out.println(!a==10);
+
+      //  System.out.println(!(a==10));
+
+
+       // System.out.println(false || false);
+
+       /* int a=400;
+        int b=200;
+        int c=300;
+
+        boolean result = a>b  && a>c;
+        System.out.println(result);*/
+
+
+       /* int a=400;
+        int b=600;
+        int c=450;
+
+        boolean result= c>a || c>b;
+        System.out.println(result);*/
+
+       /* int a=12;
+        boolean b= a>10 && a++<5;
+        System.out.println(a); //13
+        System.out.println(b); //false */
+
+      /*  int a=12;
+        boolean b= a>15 && a++<5;
+        System.out.println(a); //12
+        System.out.println(b); //false*/
+
+
+
+        int a=12;
+        boolean b= a>15 || a++<5;
+        System.out.println(a); //13
+        System.out.println(b); //true
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
