6af7424903fff8df40eed61fe6154f39d3c7e592
abhinav-fairsettle <abhinavxeon@gmail.com>
2024-10-29 13:05:40 +0530

    tm

---
+++ java/out/production/java/bs/ClassObjectExample.java
@@ -0,0 +1,42 @@
+package out.production.java.bs;
+
+public class ClassObjectExample {
+
+    public static void main(String[] args) {
+
+        Customer abhinavx = new Customer();
+        Customer rohan = new Customer();
+        abhinavx.name="abhinav kumaer singh";
+        abhinavx.show();
+        rohan.name="rohan";
+        rohan.show();
+
+    }
+
+
+
+}
+
+
+class Customer{
+
+    int cid;
+    String name;
+    long phone;
+    
+
+    void show(){
+        System.out.println("cid ="+cid);
+        System.out.println("name ="+name);
+        System.out.println("phone ="+phone);
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "cid=" + cid +
+                ", name='" + name + '\'' +
+                ", phone=" + phone +
+                '}';
+    }
+}
